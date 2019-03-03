e2311b39e114e98a753cba630e157c63062d5c08
Daniel Coelho <daniel.coelho@4dpipeline.com>
2019-03-03 11:58:37 -0300

    Add warmup and trials; Diagonal stripe to test image

--- DistanceTransformCUDA.cu
+++ DistanceTransformCUDA.cu
@@ -45,13 +45,12 @@ __global__ void edt_cols(T* d_input, T* d_output, uint width, uint height)
 
 	// Initialize val to either 0 or 'infinity'
 	T val = (1-d_input[x]) * (width+height);
-
 	g[0] = val;
 
 	// Scan 1
 	for (uint y = 1; y < height; y++)
 	{
-		val = (1-d_input[y * width + x]) * (1 + val);
+		val = (1 - d_input[y * width + x]) * (1 + val);
 		g[y] = val;
 	}
 
@@ -83,10 +82,10 @@ __global__ void edt_rows(T* d_output, uint width, uint height)
 	extern __shared__ T d_localG[];
 
 	for (uint i = threadIdx.x; i < width; i += blockDim.x)
-		d_localG[i] = d_output[y * width + i];
+		d_localG[i] = d_output[y * width + i]; 
 
-	__syncthreads();
-	
+	__syncthreads();	
+
 	T minDist = FLT_MAX;
 	for (uint i = 0; i < width; i++)
 	{
@@ -118,15 +117,28 @@ void runCUDA(T* h_inData, T* h_outData, uint width, uint height)
 	dim3 rowsGrid(ceil((1.0f*width) / threadsPerBlock), height, 1);
 	dim3 rowsThreads(min(width, 1024), 1, 1);
 
-	auto start = std::chrono::high_resolution_clock::now();
+	uint numtrials = 10000;
+
+	// Warmup
+	for (int i = 0; i < (numtrials / 10); i++)
 	{
 		edt_cols<<<colsGrid, colsThreads, height * sizeof(T)>>>(d_inData, d_outData, width, height);
 		edt_rows<<<rowsGrid, rowsThreads, width * sizeof(T)>>>(d_outData, width, height);
 		eee(cudaDeviceSynchronize());
 	}
+
+	auto start = std::chrono::high_resolution_clock::now();
+	{
+		for (int i = 0; i < numtrials; i++)
+		{
+			edt_cols<<<colsGrid, colsThreads, height * sizeof(T)>>>(d_inData, d_outData, width, height);
+			edt_rows<<<rowsGrid, rowsThreads, width * sizeof(T)>>>(d_outData, width, height);
+			eee(cudaDeviceSynchronize());
+		}
+	}
 	auto duration = std::chrono::high_resolution_clock::now() - start;
 	long long ms = std::chrono::duration_cast<std::chrono::microseconds>(duration).count();
-	printf("runCUDA executed in %lld microseconds\n", ms);    
+	printf("runCUDA executed in %lld microseconds\n", ms / numtrials);    
     
 	eee(cudaGetLastError());
 	eee(cudaMemcpy(h_outData, d_outData, numBytes, cudaMemcpyDeviceToHost)); 
@@ -152,6 +164,7 @@ int main(int argc, char **argv)
 		for (uint y = 0; y < height; y++)
 		{
 			inputData[y * width + x] = (float)(x > 100 && x < 150 && y > 100 && y < 150 ? 1.0f : 0.0f); 
+			inputData[y * width + x] = (float)(inputData[y * width + x] || abs((float)(x - y)) < 3 ? 1.0f : 0.0f);
 		}
 	}
 
